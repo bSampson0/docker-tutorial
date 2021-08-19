444bd46d166d9b973cbf86e77f29a63bce2ffd93
besampson0 <bnicerrr@gmail.com>
2021-08-19 14:26:39 -0700

    force deploy

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          testing...
+          This is react with Travis CI.
         </p>
         <a
           className="App-link"
